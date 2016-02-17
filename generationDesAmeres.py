ecf61d74bc39fa898bd74e6fd46de598e5ca4ef3
14roiron <yohann.roiron@mines-paristech.fr>
2016-02-17 12:04:48 +0100

    ajout du generateur des ameres aleatoires

---
+++ generationDesAmeres.py
@@ -0,0 +1,13 @@
+import sys
+import pickle
+from random import randint
+
+
+number=sys.argv[1]
+l=[]
+for i in range(50):
+    l.append([randint(-15,15),randint(-5,25)])
+print(l)
+Fichier = open('data'+str(number)+'.txt','wb')
+pickle.dump(l,Fichier) 
+Fichier.close()
