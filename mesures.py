e922944b451df845116158bce1d9857c52522d0b
MaximeBrt <maxime.brunet@mines-paristech.fr>
2016-02-17 14:17:36 +0100

    Mesures

--- mesures.py
+++ mesures.py
@@ -7,7 +7,14 @@ def mesureExacte(vect,amer):
 	v=[ro,np.arctan2((vect[1]+amer[1]),(vect[0]+amer[0]))*180/np.pi];
 	return v
 
+def mesureBruites(vect,amer):
+	v = [];
+	ro = bruit(np.sqrt(np.power(vect[0]-amer[0],2)+np.power(vect[1]-amer[1],2)),0,0.5);
+	v=bruit([ro,np.arctan2((vect[1]+amer[1]),(vect[0]+amer[0]))*180/np.pi],0,0.5);
+	return v
+
+def bruit(a,mu,sigma):
+	return a+np.random.normal(mu,sigma);
+
 if __name__ == '__main__':
-	v =[];
-	v = mesureExacte([0,0],[-1,1]);
-	print(str(v[0])+" "+str(v[1]))
+	pass
