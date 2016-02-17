42ca1767f52d1c72f1d81b32781b888b579b942a
MaximeBrt <maxime.brunet@mines-paristech.fr>
2016-02-17 12:45:17 +0100

    Mesures
    mesuresExacte(état_exacte_au_temps_t,amer_exacte_i)

--- mesures.py
+++ mesures.py
@@ -2,11 +2,12 @@ import numpy as np
 import matplotlib.pyplot as pyplot
 
 def mesureExacte(vect,amer):
+	v = [];
 	ro = np.sqrt(np.power(vect[0]-amer[0],2)+np.power(vect[1]-amer[1],2));
-	if vect[0]==amer[0]:
-		return [ro,0]
-	return  [ro,np.arctan2((vect[1]-amer[1])/(vect[0]-amer[0]))]
+	v=[ro,np.arctan2((vect[1]+amer[1]),(vect[0]+amer[0]))*180/np.pi];
+	return v
 
 if __name__ == '__main__':
-	v = mesureExacte([0,0],[1,0]);
+	v =[];
+	v = mesureExacte([0,0],[-1,1]);
 	print(str(v[0])+" "+str(v[1]))
