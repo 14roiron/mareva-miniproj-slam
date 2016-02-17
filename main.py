d22335e851d36a4ad411df9544c8b70baafa819d
14roiron <yohann.roiron@mines-paristech.fr>
2016-02-17 12:05:18 +0100

    premiere integration

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import actualisation
+import graphique
+import sys
+import pickle
 
 
-deplacement_globsl=[]
+
+number=sys.argv[1]
+Fichier=open('data'+str(number)+'.txt','rb')
+ameres=pickle.load(Fichier)
+Fichier.close()
+
+deplacement_global=[]
 
 
 if(__name__=="__main__"):
     #CalculerPositions()
     #CalculerCapteurs()
     #Dessiner()
-
+    pass
+    
