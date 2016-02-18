26e1e74c07db1724331c6059a3b378b2b75d8377
14roiron <yohann.roiron@mines-paristech.fr>
2016-02-18 09:39:23 +0100

    maj

--- miseajour.py
+++ miseajour.py
@@ -2,12 +2,12 @@ import numpy as np
 
 
 
-def MatriceHk(VectEtat,Ameres):
-    taille=len(VectEtat)+len(Ameres)
+def MatriceHk(Xk,Ameres):
+    taille=len(Xk)+len(Ameres)
     matrice=[]
     matrice.append([])
-    xk=VectEtat[0]
-    yk=VectEtat[1]
+    xk=Xk[0]
+    yk=Xk[1]
     for i in range(len(Ameres)):
        matrice[-1].append((-xk)/(2*sqrt((xk-Ameres[i][0])**2+(yk-Ameres[i][1])**2)))
        matrice[-1].append(((Ameres[i][1]-yk)/(Ameres[i][0]-xk)**2)/(1+((Ameres[1]-yk)/(Ameres[0]-xk))**2))
