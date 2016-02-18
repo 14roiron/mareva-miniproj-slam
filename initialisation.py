07e2cf5ff39bba0a1d4e6a7a3db58d1810fbf7ce
MaximeBrt <maxime.brunet@mines-paristech.fr>
2016-02-18 11:32:17 +0100

    Initialisation de Pk

---
+++ initialisation.py
@@ -0,0 +1,24 @@
+import numpy as np
+import mesures as me
+import prediction as pre
+
+def touslesamers(Z0):
+	global sigmaalpha
+	global sigmarho
+	P = [0.]*(3+np.size(Z0));
+	for i in range(0,3+np.size(Z0)):
+		P[i] = [0.]*(3+np.size(Z0));
+	P = pre.toMatrix(P);
+	J = [0.]*(np.size(Z0)/2);
+	R = pre.toMatrix([[np.power(sigmarho,2) , 0],[0 , np.power(sigmaalpha,2)]]);
+	for i in range(0,np.size(Z0)/2):
+		J[i] = pre.toMatrix([[np.cos(Z0[2*i+1]) , np.sin(Z0[2*i+1])],[-Z0[2*i]*np.sin(Z0[2*i+1]) , Z0[2*i]*np.cos(Z0[2*i+1])]]);
+		Q = J[i]*R*np.transpose(J[i]);
+		P[3+i,3+i] = Q[0,0];
+		P[3+i,4+i] = Q[0,1];
+		P[4+i,3+i] = Q[1,0];
+		P[4+i,4+i] = Q[1,1];
+	return P
+
+if __name__ == '__main__':
+	pass
